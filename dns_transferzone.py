5c356e4a2b3809b1ae633ed9ff083ea6faa2577b
ubrito <unaldo.brito@gmail.com>
2020-09-10 06:02:56 -0300

    Adicionada verificação de nome ou IP

--- dns_transferzone.py
+++ dns_transferzone.py
@@ -1,8 +1,26 @@
+import argparse
 import dns.query
 import dns.zone
-import argparse as ap
+import re
+import socket
+
+def verify_ip_or_name(server):
+    
+    pattern = r"(((1[0-9]|[1-9]?)[0-9]|2([0-4][0-9]|5[0-5]))\.){3}((1[0-9]|[1-9]?)[0-9]|2([0-4][0-9]|5[0-5]))"
+    validate_ip = re.match(pattern, server)
+    
+    if validate_ip:
+        return server
+    else:
+        try:
+            ip = socket.gethostbyname(server)
+            return ip
+        except Exception:
+            return False
+
 
 def transfer_zone(server, domain):
+    
     try:
         return dns.zone.from_xfr(dns.query.xfr(server, domain))
     except Exception as e:
@@ -11,17 +29,21 @@ def transfer_zone(server, domain):
 
 if __name__ == '__main__':
 
-    parser = ap.ArgumentParser(description="Busca e transfere Zonas DNS dos Domínios dados")
+    parser = argparse.ArgumentParser(description="Busca e transfere Zonas DNS dos Domínios dados")
     parser.add_argument('--server', help='O servidor DNS a ser consultado.', required=True)
     parser.add_argument('--domain', help='O domínio a ser usado na consulta.', required=True)
     parsed = parser.parse_args()
 
-    results = transfer_zone(parsed.server, parsed.domain)
+    if verify_ip_or_name(parsed.server):
+        results = transfer_zone(verify_ip_or_name(parsed.server), parsed.domain)
 
-    if not results:
-        print(results)
-        print('Não foi possível transferir a zona.')
+        if not results:
+            print(results)
+            print('Não foi possível transferir a zona.')
+        else:
+            for name in results.nodes.keys():
+                print(results[name].to_text(name))
+    
     else:
-        for name in results.nodes.keys():
-            print(results[name].to_text(name))
+        print("Servidor não existe")
 
